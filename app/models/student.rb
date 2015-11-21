9c758ac3eb46e94ebe4cf7aa3406ca1b1efb9b80
Dudisek <peta.duda@gmail.com>
2015-11-21 09:23:03 +0800

    Validation: Email + age, missing just phones

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,25 @@
 require_relative '../../db/config'
+require 'date'
 
 class Student < ActiveRecord::Base
+	attr_reader :age, :name
+	# validates :email, :uniqueness
+	validates :age, :numericality => {greater_than: 4}
+	validates :email, :uniqueness => true
+	validates :email, :format => { :with => /\w@\w{1}+[.]\w{2}/}
 # implement your Student model here
+	def name
+		@name = first_name + " " + last_name
+	end
+
+	def age
+		now = Date.today.year
+		@age = now - (birthday.year)
+	end	
 end
+
+# student = Student.find 4
+# student.name
+# student.age
+
+# p Student.all
