04d9acb55b7a5022a51ae5c8101bee92f72d1633
Dudisek <peta.duda@gmail.com>
2015-11-21 10:29:39 +0800

    Valid successfully including Advanced Validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,10 +4,11 @@ require 'date'
 class Student < ActiveRecord::Base
 	attr_reader :age, :name
 	validates :age, :numericality => {greater_than: 4}
-	validates :email, :uniqueness => true
-	validates :email, :format => { :with => /\w@\w{1}+[.]\w{2}/}
-	validates :phone, :format => { :with => /\d{3}[-]\d{3}[-]\d{4}/}
-# implement your Student model here
+	validates :email, :format => { :with => /\w@\w{1}+[.]\w{2}/}, :uniqueness => true
+	validate :valid_phone? 
+	# validates :phone, :format => { :with => /\D\d{3}\D{2}\d{3}\D\d{4}\D{2}\d{4}/, :message "Invalid phone number!"}
+
+	# implement your Student model here
 	def name
 		@name = first_name + " " + last_name
 	end
@@ -16,6 +17,13 @@ class Student < ActiveRecord::Base
 		now = Date.today.year
 		@age = now - (birthday.year)
 	end	
+
+	def valid_phone?
+		if self.phone.scan(/\d+/).join.length < 10
+			errors.add(:phone, "Must contain at least 10 digits.")
+		end
+	end
+
 end
 
 # student = Student.find 4
